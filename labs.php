c7cbab0c0f09b9bb6e65b26647c1533a61ad0900
Md Yasin <83154902+mdyasin399@users.noreply.github.com>
2025-08-02 23:17:29 +0600

    Information about lab infrastructure

---
+++ labs.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Labs | RM506</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon" />
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Handlee&family=Nunito&display=swap" rel="stylesheet" />
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet" />
+
+    <!-- Flaticon Font -->
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet" />
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Navbar Start -->
+    <div class="container-fluid bg-light position-relative shadow">
+        <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0 px-lg-5">
+            <a href="index.php" class="navbar-brand font-weight-bold text-secondary" style="font-size: 50px">
+                <i class="fas fa-flask text-primary me-2"></i>
+                <span class="text-primary">RM506</span>
+            </a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                <div class="navbar-nav font-weight-bold mx-auto py-0">
+                    <a href="index.php" class="nav-item nav-link active">Home</a>
+                    <a href="about.php" class="nav-item nav-link">About</a>
+                    <a href="labs.php" class="nav-item nav-link">Labs</a>
+                    <a href="requests.php" class="nav-item nav-link">Requests</a>
+                    <a href="teachers.php" class="nav-item nav-link">Teachers</a>
+                    <a href="shopping.php" class="nav-item nav-link">Shopping</a>
+                    <a href="reviews.php" class="nav-item nav-link">Reviews</a>
+                    <a href="contact.php" class="nav-item nav-link">Contact</a>
+                </div>
+                <a href="login.php" class="btn btn-primary px-4">Login</a>
+                <a href="signup.php" class="btn btn-primary px-4 ml-2">Sign up</a>
+            </div>
+        </nav>
+    </div>
+    <!-- Navbar End -->
+
+
+    <!-- Class Start -->
+    <div class="container-fluid pt-5">
+        <div class="container">
+            <div class="text-center pb-2">
+                <p class="section-title px-5">
+                    <span class="px-2">Popular Lab Rooms</span>
+                </p>
+                <h1 class="mb-4">Practise for Your Upcoming Project Shows</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 mb-5">
+                    <div class="card border-0 bg-light shadow-sm pb-2">
+                        <img class="card-img-top mb-2" src="img/class-1.jpg" alt=""
+                            style="width: 100%; height: 200px; object-fit: cover;" />
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Chemistry Lab</h4>
+                            <p class="card-text">
+                            </p>
+                        </div>
+                        <div class="card-footer bg-transparent py-4 px-5">
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Lab Assistant</strong>
+                                </div>
+                                <div class="col-6 py-1">Hasnat Zaman</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Room No</strong>
+                                </div>
+                                <div class="col-6 py-1">504</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Free Time</strong>
+                                </div>
+                                <div class="col-6 py-1">08:00 - 10:30</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Tables</strong>
+                                </div>
+                                <div class="col-6 py-1">7</div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Seats</strong>
+                                </div>
+                                <div class="col-6 py-1">28</div>
+                            </div>
+                        </div>
+                        <a href="login.php" class="btn btn-primary px-4 mx-auto mb-4">Book Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-5">
+                    <div class="card border-0 bg-light shadow-sm pb-2">
+                        <img class="card-img-top mb-2" src="img/class-2.jpg" alt=""
+                            style="width: 100%; height: 200px; object-fit: cover;" />
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Computer Lab</h4>
+                            <p class="card-text">
+                            </p>
+                        </div>
+                        <div class="card-footer bg-transparent py-4 px-5">
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Lab Assistant</strong>
+                                </div>
+                                <div class="col-6 py-1">Md. Ariful Hasan Opu</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Room No</strong>
+                                </div>
+                                <div class="col-6 py-1">926</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Free Time</strong>
+                                </div>
+                                <div class="col-6 py-1">11:00 - 01:30</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Tables</strong>
+                                </div>
+                                <div class="col-6 py-1">6</div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Seats</strong>
+                                </div>
+                                <div class="col-6 py-1">24</div>
+                            </div>
+                        </div>
+                        <a href="login.php" class="btn btn-primary px-4 mx-auto mb-4">Book Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-5">
+                    <div class="card border-0 bg-light shadow-sm pb-2">
+                        <img class="card-img-top mb-2" src="img/class-3.jpg" alt=""
+                            style="width: 100%; height: 200px; object-fit: cover;" />
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Electronics Lab</h4>
+                            <p class="card-text">
+                            </p>
+                        </div>
+                        <div class="card-footer bg-transparent py-4 px-5">
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Lab Assistant</strong>
+                                </div>
+                                <div class="col-6 py-1">Rakibul Islam</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Room No</strong>
+                                </div>
+                                <div class="col-6 py-1">505</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Free Time</strong>
+                                </div>
+                                <div class="col-6 py-1">02:00 - 04:30</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Tables</strong>
+                                </div>
+                                <div class="col-6 py-1">7</div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Seats</strong>
+                                </div>
+                                <div class="col-6 py-1">28</div>
+                            </div>
+                        </div>
+                        <a href="login.php" class="btn btn-primary px-4 mx-auto mb-4">Book Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-5">
+                    <div class="card border-0 bg-light shadow-sm pb-2">
+                        <img class="card-img-top mb-2" src="img/Organic Pharmacy.jpg" alt=""
+                            style="width: 100%; height: 200px; object-fit: cover;" />
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Organic Pharmacy</h4>
+                            <p class="card-text">
+                            </p>
+                        </div>
+                        <div class="card-footer bg-transparent py-4 px-5">
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Lab Assistant</strong>
+                                </div>
+                                <div class="col-6 py-1">Hasnat Zaman</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Room No</strong>
+                                </div>
+                                <div class="col-6 py-1">725</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Free Time</strong>
+                                </div>
+                                <div class="col-6 py-1">11:00 - 01:30</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Tables</strong>
+                                </div>
+                                <div class="col-6 py-1">5</div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Seats</strong>
+                                </div>
+                                <div class="col-6 py-1">20</div>
+                            </div>
+                        </div>
+                        <a href="login.php" class="btn btn-primary px-4 mx-auto mb-4">Book Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-5">
+                    <div class="card border-0 bg-light shadow-sm pb-2">
+                        <img class="card-img-top mb-2" src="img/physiology.jpg" alt=""
+                            style="width: 100%; height: 200px; object-fit: cover;" />
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Physiology Lab</h4>
+                            <p class="card-text">
+                            </p>
+                        </div>
+                        <div class="card-footer bg-transparent py-4 px-5">
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Lab Assistant</strong>
+                                </div>
+                                <div class="col-6 py-1">Md. Ariful Hasan Opu</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Room No</strong>
+                                </div>
+                                <div class="col-6 py-1">930</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Free Time</strong>
+                                </div>
+                                <div class="col-6 py-1">02:00 - 04:30</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Tables</strong>
+                                </div>
+                                <div class="col-6 py-1">7</div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Seats</strong>
+                                </div>
+                                <div class="col-6 py-1">28</div>
+                            </div>
+                        </div>
+                        <a href="login.php" class="btn btn-primary px-4 mx-auto mb-4">Book Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-5">
+                    <div class="card border-0 bg-light shadow-sm pb-2">
+                        <img class="card-img-top mb-2" src="img/Microbiology.jpg" alt=""
+                            style="width: 100%; height: 200px; object-fit: cover;" />
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Microbiology Lab</h4>
+                            <p class="card-text">
+                            </p>
+                        </div>
+                        <div class="card-footer bg-transparent py-4 px-5">
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Lab Assistant</strong>
+                                </div>
+                                <div class="col-6 py-1">Rakibul Islam</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Room No</strong>
+                                </div>
+                                <div class="col-6 py-1">622</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Free Time</strong>
+                                </div>
+                                <div class="col-6 py-1">08:00 - 10:30</div>
+                            </div>
+                            <div class="row border-bottom">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Tables</strong>
+                                </div>
+                                <div class="col-6 py-1">8</div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 py-1 text-right border-right">
+                                    <strong>Seats</strong>
+                                </div>
+                                <div class="col-6 py-1">32</div>
+                            </div>
+                        </div>
+                        <a href="login.php" class="btn btn-primary px-4 mx-auto mb-4">Book Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Class End -->
+
+
+
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
